e3c1eb827aa0320772efc8798d330e032cb5b749
zhangsibo1129 <zhangsibo1129@gmail.com>
2024-06-07 11:45:12 +0800

    add basic generation config for hf

--- src/chatfactory/llm/huggingface.py
+++ src/chatfactory/llm/huggingface.py
@@ -37,13 +37,24 @@ class HFChatModel(BaseChatModel):
         device = self.client.device
         model_inputs = self.tokenizer([raw_inputs], return_tensors="pt").to(device)
         return model_inputs
+    
+    def _add_basic_config(self, generation_config: Dict) -> Dict:
+        generation_config.update(
+            dict(
+                do_sample = True,
+                max_length = 100,
+            )
+        )
+        return generation_config
+        
 
     def invoke(self, messages: List[Dict], generation_config: Optional[dict] = None) -> str:
         model_inputs = self._prepare_inputs(messages)
 
         if generation_config is None:
             generation_config = {}
-            
+        
+        generation_config = self._add_basic_config(generation_config)
         generated_ids = self.client.generate(model_inputs.input_ids, **generation_config)
         generated_ids = [
             output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
@@ -61,6 +72,7 @@ class HFChatModel(BaseChatModel):
         streamer = TextIteratorStreamer(self.tokenizer, skip_prompt=True, skip_special_tokens=True)
         generation_kwargs = dict(model_inputs, streamer=streamer)
         generation_kwargs.update(generation_config)
+        generation_kwargs =  self._add_basic_config(generation_kwargs)
         thread = Thread(target=self.client.generate, kwargs=generation_kwargs)
         thread.start()
         return streamer
