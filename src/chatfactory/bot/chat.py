6ff539d881431f28f54378d7a4f03e48f2a1ee96
zhangsibo1129 <zhangsibo1129@gmail.com>
2024-06-26 10:54:00 +0800

    refactor bot

--- src/chatfactory/bot/chat.py
+++ src/chatfactory/bot/chat.py
@@ -1,39 +1,11 @@
-from typing import Optional, Dict, List, Tuple
+from typing import Optional, Dict, List, Tuple, Any
 from chatfactory.llm.utils import LLM_REGISTRY
-from chatfactory.log import logger
+from chatfactory.llm.llm import LLM
+from chatfactory.bot.utils import BaseChatBot
 
 
-class SimpleChatBot:
-    def __init__(self, llm_config: Optional[dict] = None) -> None:
-        llm_engine, model, model_config = self.parse_llm_config(llm_config)
-        self.setup_model(llm_engine, model, model_config)
-
-    def parse_llm_config(
-        self, llm_config: Optional[dict] = None
-    ) -> Tuple[Optional[str], Optional[str], Optional[dict]]:
-        if llm_config is None:
-            llm_config = {}
-        llm_engine = llm_config.get("engine", None)
-        model = llm_config.get("model", None)
-        model_config = llm_config.get("model_config", None)
-        return llm_engine, model, model_config
-
-    def setup_model(
-        self,
-        llm_engine: Optional[str] = None,
-        model: Optional[str] = None,
-        model_config: Optional[dict] = None,
-    ) -> None:
-        logger.info("Setting up LLM...")
-        if llm_engine is None:
-            llm_engine = "openai"
-        llm_cls = LLM_REGISTRY[llm_engine]
-        self.llm = llm_cls(model, model_config)
-        logger.info(f"LLM Engine: {llm_engine}")
-        logger.info(f"Model ID/Path: {self.llm.model}")
-        logger.info(f"Model Config: {model_config}")
-        logger.info("LLM has been initialized.")
-
+class SimpleChatBot(BaseChatBot):
+    """A simple chat bot that uses a single LLM."""
     def chat(
         self,
         message: str,
@@ -41,7 +13,7 @@ class SimpleChatBot:
         system_prompt: Optional[str] = None,
         generation_config: Optional[Dict] = None,
         stream: bool = True,
-    ) -> str:
+    ) -> Any:
         messages = []
         if system_prompt:
             messages.append({"role": "system", "content": system_prompt})
--- src/chatfactory/bot/chat_with_arxiv.py
+++ src/chatfactory/bot/chat_with_arxiv.py
@@ -1,6 +1,7 @@
 import json
 from typing import Optional, Dict, List, Tuple, Any
 from chatfactory.llm.utils import LLM_REGISTRY
+from chatfactory.bot.utils import BaseChatBot
 from chatfactory.tool.utils import TOOL_REGISTRY
 from chatfactory.log import logger
 
@@ -53,37 +54,11 @@ PAPER_CARD_MARKDOWN = """
 """
 
 
-class ArxivChatBot:
+class ArxivChatBot(BaseChatBot):
+
     def __init__(self, llm_config: Optional[dict] = None) -> None:
-        llm_engine, model, model_config = self.parse_llm_config(llm_config)
-        self.setup_model(llm_engine, model, model_config)
+        super().__init__(llm_config)
         self.tool = TOOL_REGISTRY["arxiv"]()
-
-    def parse_llm_config(
-        self, llm_config: Optional[dict] = None
-    ) -> Tuple[Optional[str], Optional[str], Optional[dict]]:
-        if llm_config is None:
-            llm_config = {}
-        llm_engine = llm_config.get("engine", None)
-        model = llm_config.get("model", None)
-        model_config = llm_config.get("model_config", None)
-        return llm_engine, model, model_config
-
-    def setup_model(
-        self,
-        llm_engine: Optional[str] = None,
-        model: Optional[str] = None,
-        model_config: Optional[dict] = None,
-    ) -> None:
-        logger.info("Setting up LLM...")
-        if llm_engine is None:
-            llm_engine = "openai"
-        llm_cls = LLM_REGISTRY[llm_engine]
-        self.llm = llm_cls(model, model_config)
-        logger.info(f"LLM Engine: {llm_engine}")
-        logger.info(f"Model ID/Path: {self.llm.model}")
-        logger.info(f"Model Config: {model_config}")
-        logger.info("LLM has been initialized.")
 
     def _search_from_arxiv(
         self,
